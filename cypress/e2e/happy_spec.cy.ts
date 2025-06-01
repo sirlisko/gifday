4505bbe1e00bd4a565567a29588c237bd24a4ace
Luca Lischetti <luca@sirlisko.com>
2025-06-01 16:23:14 +0100

    chore: fixed cypress

--- cypress/e2e/happy_spec.cy.ts
+++ cypress/e2e/happy_spec.cy.ts
@@ -1,14 +1,16 @@
 describe("The Web App", () => {
 	it("successfully selected a gif", () => {
-		cy.visit("http://localhost:3000");
-		cy.get('[role="button"]').first().click();
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		cy.visit(Cypress.config("baseUrl")!);
+		cy.get('[type="button"]').first().click();
 		cy.get('input[type="text"]').type("pizza");
 		cy.get("form").submit();
-		cy.get('[class*="modal"] video');
+		cy.get("video").should("be.visible");
 	});
 
 	it.skip("successfully show the gif on hover an img", () => {
-		cy.visit("http://localhost:3000");
+		// biome-ignore lint/style/noNonNullAssertion: <explanation>
+		cy.visit(Cypress.config("baseUrl")!);
 		cy.get('[class*="StyledDay"]').first().click();
 		cy.get('input[type="text"]').type("pizza");
 		cy.get("form").submit();
